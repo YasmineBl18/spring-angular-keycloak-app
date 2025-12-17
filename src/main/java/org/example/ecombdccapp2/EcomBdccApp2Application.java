aa66704deea1467ab9febf8e9fbdf7180f228c9e
Yasmine Bourijal <yasbourijal@gmail.com>
2025-12-17 19:51:18 +0100

    Update README

---
+++ src/main/java/org/example/ecombdccapp2/EcomBdccApp2Application.java
@@ -0,0 +1,13 @@
+package org.example.ecombdccapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBdccApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomBdccApp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ecombdccapp2/EcomBdccApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecombdccapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBdccApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
